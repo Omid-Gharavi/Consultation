cee2cc184dcd28b77b381af0d498e3b0baa93d12
OMID GHARAVI <omidbros3@gmail.com>
2024-06-23 17:34:56 +0330

    component: Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
+import Image from "next/image";
+import Hero from '@/images/Hero.svg'
+import Header from "@/components/header/Header";
+
 export default function Home() {
   return (
     <main>
-      Home
+      <div className="relative min-h-screen">
+        <Header />
+        <Image className="absolute" fill objectFit="cover" src={Hero} />
+      </div>
     </main>
   );
 }
