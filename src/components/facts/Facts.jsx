873b8b4ab25a8d69be72602fc1f2426ca2eb6f1c
OMID GHARAVI <omidbros3@gmail.com>
2024-06-27 13:00:06 +0330

    facts component

--- src/components/facts/Facts.jsx
+++ src/components/facts/Facts.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
 const Facts = () => {
+    const lists = [
+        {
+            text: '1M',
+            des: 'Customers visit Albino every months',
+        },
+        {
+            text: '93%',
+            des: 'Satisfaction rate from our customers.',
+        },
+        {
+            text: '4.9',
+            des: 'Average customer ratings out of 5.00!',
+        },
+    ]
+
     return (
-        <div>Facts</div>
+        <div className='mt-20'>
+            <div className='flex flex-wrap justify-center gap-24'>
+                {
+                    lists.map((list, index) => (
+                        <div className='flex items-center gap-[30px]' key={index + 1}>
+                            <p className='text-5xl font-bold'>{list.text}</p>
+                            <p className='w-[174px] text-zinc-600'>{list.des}</p>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className='mt-[75px] h-1 bg-[#E7E9ED]'></div>
+        </div>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from '@/images/Hero.svg'
 import HeroSection from '@/components/hero/HeroSection'
 import Header from "@/components/header/Header";
+import Facts from "@/components/facts/Facts";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <HeroSection />
         <Image className="absolute -z-10" fill objectFit="cover" src={Hero} />
       </div>
-      sdffffffffffffffffffffffff
+      <Facts />
     </main>
   );
 }
